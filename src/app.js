a7c8aee48df6ac8bfb53af8d2b9773c87b0339fe
noceive <nocivescience@gmail.com>
2022-11-18 03:55:03 -0300

    mi primer commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { pool } from './db.js';
+const app= express()
+app.get('/', async (req,res)=>{
+    const [rows]=await pool.query('SELECT *FROM users');
+    res.json(rows)
+})
+app.get('/ping', async (req,res)=>{
+    const [result]= await pool.query(`SELECT 'Hello Word' as RESULT`);
+    console.log(result)
+    res.send(res.json(result[0]))
+})
+app.get('/create', async (req,res)=>{
+    const result = await pool.query('INSERT INTO users(name) VALUES ("John")');
+    res.json(result)
+})
+app.listen(3000)
+console.log('Server running')
